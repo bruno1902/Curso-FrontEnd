0762450e78a384bc231e4e24f95e807fbc54276b
bruno1902 <brunodileo@live.com>
2019-02-06 16:45:09 +0000

    Memory-Card V1.1

---
+++ Jogo-da-memoria/src/index.js
@@ -0,0 +1,30 @@
+const $root = document.querySelector("#root");
+const $memoryCard = document.createElement("article");
+const $memoryCardFront = document.createElement("article");
+
+const $iconCollab = `
+<img 
+src='img/icon-collabcode.png' 
+alt='Mascote Gaio' 
+class='icon' 
+/>
+`;
+
+const $iconC = `
+<img 
+src='img/icon-c.png' 
+alt='Livro C' 
+class='icon' 
+/>
+`;
+
+$memoryCard.classList.add("memory-card");
+$root.insertBefore($memoryCard, null);
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
+$memoryCard.insertAdjacentHTML("afterbegin",$iconCollab);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin",$iconC);
